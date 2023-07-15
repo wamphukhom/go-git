df035237451a4d2fe6a1580f869a35791dc9e977
vissarud <wamphukhom@gmail.com>
2023-07-15 22:45:45 +0700

    h2

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ func sum(a, b int) int {
 	return a + b
 }
 
+func dev(a, b float64) float64 {
+	return a / b
+}
+
 func main() {
 	fmt.Println("Hello worldkkkk")
 	fmt.Println(sum(1, 2))
+	fmt.Println(dev(1, 2))
 }
